4b0a794e340d56741d99348ca4b7824f45aa94c7
Omari Brown <omaris.brown@gmail.com>
2020-07-14 23:41:31 -0400

    1.

--- src/App.js
+++ src/App.js
@@ -4,28 +4,14 @@ import axios from 'axios';
 
 
 class App extends React.Component {
-  constructor() {
-    super()
 
-    this.state = {
-      error: null,
-      isLoaded: false,
-      data: []
-    }
-  }
-
-  componentDidMount() {
-    axios.get(`http://anapioficeandfire.com/api/characters/583`)
-    .then(res => {
-      const got = res.data.data;
-      console.log()
-    })
-  }
 
   render() {
     return (
       <div className="App">
-        <h1>Game of Fetches</h1>
+        <ol>
+          <li>Highgarden</li>
+        </ol>
       </div>
     );
   }
